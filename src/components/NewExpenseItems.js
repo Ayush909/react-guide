59ad768247af62d760a53e30b41882100d50b55c
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2022-03-23 00:36:30 +0530

    card wrapper

--- src/components/NewExpenseItems.js
+++ src/components/NewExpenseItems.js
@@ -1,13 +1,14 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import './NewExpenseItems.css'
 
 function NewExpenseItems(props){
     return (
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem title = {props.expenses[0].title} date = {props.expenses[0].date} amount = {props.expenses[0].amount}/>
             <ExpenseItem title = {props.expenses[1].title} date = {props.expenses[1].date} amount = {props.expenses[1].amount}/>
             <ExpenseItem title = {props.expenses[2].title} date = {props.expenses[2].date} amount = {props.expenses[2].amount}/>
-        </div>
+        </Card>
     )
 }
 

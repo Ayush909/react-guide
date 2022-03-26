fb77adeca9eecff9b4e24a43215ca0348eec9473
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2022-03-27 00:05:59 +0530

    minor changes

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,22 @@
+import React,{useState} from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css'
 
 const Expenses = (props) =>{
-    const DateFilterHandler = (data)=>{
-        console.log(data)
+
+    const [filterDate,setFilterDate] = useState('2020');
+
+    const DateFilterHandler = (newFilterDate)=>{
+        console.log(newFilterDate)
+        setFilterDate(newFilterDate);
     }
     return (
         <div>
-            <ExpensesFilter onChangeDateFilter={DateFilterHandler}/>
+            
             <Card className="expenses">
+                <ExpensesFilter onChangeDateFilter={DateFilterHandler} filterDate={filterDate}/>
                 <ExpenseItem title = {props.expenses[0].title} date = {props.expenses[0].date} amount = {props.expenses[0].amount}/>
                 <ExpenseItem title = {props.expenses[1].title} date = {props.expenses[1].date} amount = {props.expenses[1].amount}/>
                 <ExpenseItem title = {props.expenses[2].title} date = {props.expenses[2].date} amount = {props.expenses[2].amount}/>

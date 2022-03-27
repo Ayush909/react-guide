87e77fb89a203b5eb05f34e09a0763d0126129ca
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2022-03-27 12:11:30 +0530

    dynamic loading of expenses array

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -12,12 +12,11 @@ const ExpenseItem = (props)=>{
        setTitle('Updated!');
        console.log("Clicked!!");
    }
-
     return (
         <Card className='expense-item'>
            <ExpenseDate date = {props.date} />
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>Rs.{props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,17 +9,24 @@ const Expenses = (props) =>{
     const [filterDate,setFilterDate] = useState('2020');
 
     const DateFilterHandler = (newFilterDate)=>{
-        console.log(newFilterDate)
+        // console.log(newFilterDate)
         setFilterDate(newFilterDate);
     }
+
+    const filteredExpenses = props.expenses.filter(expense => {
+        return expense.date.getFullYear().toString() === filterDate;
+    })
+
     return (
         <div>
             
             <Card className="expenses">
                 <ExpensesFilter onChangeDateFilter={DateFilterHandler} filterDate={filterDate}/>
-                <ExpenseItem title = {props.expenses[0].title} date = {props.expenses[0].date} amount = {props.expenses[0].amount}/>
-                <ExpenseItem title = {props.expenses[1].title} date = {props.expenses[1].date} amount = {props.expenses[1].amount}/>
-                <ExpenseItem title = {props.expenses[2].title} date = {props.expenses[2].date} amount = {props.expenses[2].amount}/>
+                {filteredExpenses.map(
+                    expense=> <ExpenseItem key={expense.id} title={expense.title} date={expense.date} amount={expense.amount} />
+                )}
+                
+               
             </Card>
         </div>
     )
